8c74d10e1437e6e1ddcb78bec4be2ea31676cf20
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-30 20:39:11 +0200

    basic styling navbar n hero

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import "../styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <section id="home">
+      <h1 className="hero-h1">Fullstack developer</h1>
+      <h2 className="hero-h2">Emelie Björkman</h2>
+      <p>
+        Just nu halvvägs genom Fullstack utbildning hos Chas Academy med drivet
+        att lära mig mer.
+      </p>
+      <a href="#projects" className="hero-btn">
+        Click to see my projects
+      </a>{" "}
+    </section>
+  );
+};
+
+export default Hero;
